3d1aa9f859969b9781f1f982c70ea9c232c07d2c
Karthi <57511012+karthis-code@users.noreply.github.com>
2020-01-20 20:50:49 +0000

    feat: added bundle analyzer and visualize and clean webpack

--- _webpack/webpack.common.js
+++ _webpack/webpack.common.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const Visualizer = require('webpack-visualizer-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   entry: {
     app: "./src/client/index.js"
@@ -44,6 +46,17 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "My website",
       template: path.resolve(__dirname, "../_templates/client.html")
-    })
+    }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+      analyzerMode: 'static',
+      reportFilename: path.resolve(__dirname, `../_dist/bundle.html`),
+    }),
+    new Visualizer({
+      filename: './visualizer.html',
+    }),
+    new CleanWebpackPlugin(),
   ]
 };
+
+
--- _webpack/webpack.prod.js
+++ _webpack/webpack.prod.js
@@ -2,6 +2,7 @@ const path = require('path');
 const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 
+
 module.exports = merge(commonConfig, {
   mode: 'production',
 });
