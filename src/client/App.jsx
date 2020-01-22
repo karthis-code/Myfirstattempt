0a3e71db31852f2a58c0de3e41e82876036f8a04
Karthi <57511012+karthis-code@users.noreply.github.com>
2020-01-22 21:14:28 +0000

    feat: created react component increment and decrement value

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,23 +1,51 @@
-import React, { Component } from "react";
+/*eslint-disable no-magic-numbers */
+import React from "react";
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  increment = () => {
+    const currentVal = this.state.count;
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      inputText: 'Hello Karthik',
-    };
-    this.handleInput=this.handleInput.bind(this);
+    this.setState({
+      count: currentVal + 1,
+    });
   }
-  handleInput() {
-    console.log('input capture');
+  decrement = () => {
+    const currentVal = this.state.count;
+
+    this.setState({
+      count: currentVal -1,
+    });
   }
   render() {
-    return (
-      <div>
-        <input type="text" onChange={this.handleInput} />
-        <h1>{this.state.inputText}</h1>
-        <p>Hi There, How are you</p>
-      </div>
-    );
+    return <div>
+      <p>{this.state.count}</p>
+      <button onClick={this.increment}>+</button>
+      <button onClick={this.decrement}>-</button>
+    </div>
   }
 }
+export default App
+
+//export default class App extends Component {
+//constructor() {
+//super();
+//this.state = {
+//inputText: "Hello Karthik"
+//};
+//this.handleInput = this.handleInput.bind(this);
+//}
+//handleInput() {
+// console.log("input capture");
+//}
+//render() {
+//return (
+//<div>
+// <input type="text" onChange={this.handleInput} />
+//<button>+</button>
+//<button>-</button>
+//</div>
+//);
+//}
+//}
