29a1ef1d2ccf0b2a3f5ffcbf57aa354f81cfd55c
Karthi <57511012+karthis-code@users.noreply.github.com>
2020-01-27 21:54:42 +0000

    refactor: modified firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
 const functions = require('firebase-functions');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send('hello');
+});
+
+const runtimeOpts = {
+  timeoutSeconds: 300,
+  memory: '2GB',
+};
+
+exports.myExpressApp = functions.runWith(runtimeOpts).https.onRequest(app);
